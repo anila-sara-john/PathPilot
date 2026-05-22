642837f54eba679c169eec1a9c67f5fdcd42f4b4
Anila Sara John <anilasajohn@gmail.com>
2026-05-22 11:50:21 +0530

    Update db_config.py

--- db_config.py
+++ db_config.py
@@ -4,7 +4,6 @@ def get_db_connection():
     return mysql.connector.connect(
         host="localhost",           # your MySQL host
         user="root",                # MySQL username
-        #password="YOUR_MYSQL_PASSWORD",         # MySQL password you set    # CHANGE THIS LOCALLY IN YOUR COMPUTER
-        password="root123",
+        password="YOUR_MYSQL_PASSWORD",    # MySQL password you set    # CHANGE THIS LOCALLY IN YOUR COMPUTER
         database="pathpilot_db"     # name of your database
     )
